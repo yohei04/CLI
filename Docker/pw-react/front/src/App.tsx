e2fdb2158618d7aba79ebf2457ecf0473ac65a3f
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-21 11:22:49 +0900

    add useStyles

--- Docker/pw-react/front/src/App.tsx
+++ Docker/pw-react/front/src/App.tsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { Container, CssBaseline} from '@material-ui/core';
+import { Container, CssBaseline, Grid, makeStyles } from "@material-ui/core";
+import red from "@material-ui/core/colors/red";
 
-interface Props {
+interface Props {}
 
-}
+const useStyles = makeStyles((theme) => ({
+  red: {
+    backgroundColor: red[500],
+  },
+}));
 
-const App: React.FC = props => {
+const App: React.FC = (props) => {
+  const classes = useStyles();
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xl">
       <CssBaseline />
-      hello
+      <Grid container spacing={3}>
+        <Grid item xs={3} className={classes.red}>
+          bus
+        </Grid>
+        <Grid item xs={6}>
+          main
+        </Grid>
+        <Grid item xs={3}>
+          users
+        </Grid>
+      </Grid>
     </Container>
   );
 };
