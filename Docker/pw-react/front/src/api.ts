480ccf51e178c79d6903865c01e8d8dc76859e0d
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-21 12:16:09 +0900

    fix webSocket

--- Docker/pw-react/front/src/api.ts
+++ Docker/pw-react/front/src/api.ts
@@ -10,6 +10,34 @@ export default class ChatBus {
     this.ws.onmessage = this.onMessage;
   }
 
+  public busList() {
+    this.ws.send(
+      JSON.stringify({
+        type: "bus_list",
+        msg: "",
+      })
+    );
+  }
+
+  public hitchhickerList() {
+    this.ws.send(
+      JSON.stringify({
+        type: "hitchhicker_list",
+        msg: "",
+      })
+    );
+  }
+
+  private subscribers: {
+    [key: string]: Array<(data: any) => void>;
+  } = {};
+  public subscribe(eventName: string, callback: (data: any) => void): void {
+    if (!this.subscribers[eventName]) {
+      this.subscribers[eventName] = [];
+    }
+    this.subscribers[eventName].push(callback);
+  }
+
   private onError(this: WebSocket, event: Event): any {}
   private onOpen(this: WebSocket, ev: Event): any {
     // $.event.trigger({
@@ -23,10 +51,6 @@ export default class ChatBus {
     //   message: ""
     // });
     // this.info("Connected to chatBus server ! Please type in username");
- 
- 
- 
- 
     // if (e.msg_type == "chat" && (_this.username == "" || _this.username == undefined)) {
     //   _this.info("Please select a username !");
     // };
@@ -37,7 +61,13 @@ export default class ChatBus {
     // console.log("sending ", Message);
     // _this.ws.send(JSON.stringify(Message));
   }
-  private onMessage(this: WebSocket, ev: Event): any {
+  private onMessage(this: WebSocket, ev: MessageEvent): any {
+    const data = JSON.parse(ev.data);
+    // dataをpublishする
+    // this.subscribers[data.type].forEach(cb => {
+    //   cb(data)
+    // })
+
     // var Data = JSON.parse(e.data);
     // console.log(Data);
     // $.event.trigger({
