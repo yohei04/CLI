407abe50f97471c270b3e342499b6e23c4d12c77
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-23 13:34:50 +0900

    add products controller

---
+++ Docker/deanin_todo_api/test/controllers/products_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get products_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get products_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get products_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get products_destroy_url
+    assert_response :success
+  end
+
+end
