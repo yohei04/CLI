16b02a723e4d53b6c7eb733b21470c35e0d8ea54
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-23 16:13:38 +0900

    fix products controller

--- Docker/deanin_todo_api/app/controllers/products_controller.rb
+++ Docker/deanin_todo_api/app/controllers/products_controller.rb
@@ -1,13 +1,28 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+    render json: @products
   end
 
   def create
+    @product = Product.create(products: params[:product])
+    render json: @product
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product: params[:product])
+    render json: @product
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      head :no_content, status: :ok
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+    
+
   end
 end
