3d16c3df31f451a50345dfb94ce6e4c203f75d8a
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-24 16:14:50 +0900

    fetch API data

--- Docker/deanin_todo_api/front/app/src/App.tsx
+++ Docker/deanin_todo_api/front/app/src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
 
+const App = () => {
+  const [products, setProducts] = useState([])
 
-function App() {
+  const url = 'http://localhost:3000/products';
+
+  useEffect(() => {
+    axios.get(url).then((result) => {
+      console.log(result)
+      setProducts(result.data);
+    })
+    .catch(err => console.log(err)) ;
+  }, [])
+
   return (
     <div className="App">
       <h1>Hello</h1>
+      <div>
+        {products.map((product: any) => (
+          <ul key={product.id}>
+            <p>種類: {product.product}</p>
+            <p>誕生日: {product.created_at}</p>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
