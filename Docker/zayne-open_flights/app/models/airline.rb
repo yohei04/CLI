5ee209516d7ddbe7b009c9f76b30381c934a2336
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-28 01:57:24 +0900

    add some method in airline.rb

--- Docker/zayne-open_flights/app/models/airline.rb
+++ Docker/zayne-open_flights/app/models/airline.rb
@@ -1,2 +1,13 @@
 class Airline < ApplicationRecord
+  has_many :reviews
+
+  before_create :slugfy
+
+  def slugfy
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    reviews.average(:score).round(2).to_f    
+  end
 end
