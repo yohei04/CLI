e3802c4b5b529686d269f1f3af1b671cd69432c6
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-29 16:49:04 +0900

    update Airlines componenst

--- Docker/zayne-open_flights/app/javascript/components/Airlines/Airlines.js
+++ Docker/zayne-open_flights/app/javascript/components/Airlines/Airlines.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Airline from './Airline';
 
 const Airlines = () => {
+  const [airlines, setAirlines] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/v1/airlines.json')
+      .then((res) => {
+        setAirlines(res.data.data);
+      })
+      .catch((res) => console.log(res));
+  }, [airlines.length]);
+
+  const grid = airlines.map((item) => (
+    <Airline key={item.attributes.name} attributes={item.attributes}></Airline>
+  ));
+
+  console.log(airlines);
+
   return (
-    <div>
-      This is the Airlines#index view for our app.
+    <div className="home">
+      <div className="header">
+        <h1>OpenFlights</h1>
+        <div className="subheader">Honest, unbiased airline reviews.</div>
+      </div>
+      <div className="grid">
+        <ul>{grid}</ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Airlines
-
+export default Airlines;
