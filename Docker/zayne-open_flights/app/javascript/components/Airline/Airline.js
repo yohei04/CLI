6d4acf23d103ca78ca098d7d6383c23a027271cd
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-07-30 10:55:20 +0900

    create Airline component

--- Docker/zayne-open_flights/app/javascript/components/Airline/Airline.js
+++ Docker/zayne-open_flights/app/javascript/components/Airline/Airline.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import axios from 'axios';
 
-const Airline = () => {
-  return <div>This is the Airlines#show view for our app.</div>;
+const Airline = (props) => {
+  const [airline, setAirline] = useState({});
+  const [review, setReview] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    const url = `/api/v1/airlines/${slug}`;
+
+    axios
+      .get(url)
+      .then((res) => {
+        setAirline(res.data);
+        setLoaded(true);
+      })
+      .catch((res) => console.log(res));
+  }, []);
+
+  console.log(airline);
+
+  return (
+    <div className="wrapper">
+      <div className="column">
+        {loaded && <Header attributes={airline.data.attributes} reviews={airline.included} />}
+        <div className="reviews"></div>
+      </div>
+      <div className="column">
+        <div className="review-form">[Review Form Goes Here.]</div>
+      </div>
+    </div>
+  );
 };
 
 export default Airline;
